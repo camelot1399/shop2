4f36b79383d89fa6a5d49ab5a7c095bf4dce7167
camelot1399 <camelot1399@mail.ru>
2021-04-30 11:56:17 +0300

    update menu

--- js/main.js
+++ js/main.js
@@ -1,17 +1,45 @@
 let menu = {
     init() {
         let btnMenu = document.querySelector('#menuBtn');
+        
         let btnCloseMenu = document.querySelector('.menu__button');
         let menu__backGr = document.querySelector('.menu__backGr');
-        console.log(btnCloseMenu);
-        console.log(btnMenu);
+
+        btnMenu.addEventListener('click', () => {
+            this._handle();
+        });
+
         btnCloseMenu.addEventListener('click', () => {
-            btnMenu.click();
+            this._handle();
         });
 
         menu__backGr.addEventListener('click', () => {
-            btnMenu.click();
+            this._handle();
         });
+    },
+    _handle() {
+        let menuBox = document.querySelector('.menuBox');
+        let attr = menuBox.getAttribute('view');
+        let menu = document.querySelector('.menu');
+
+        if (attr === 'close') {
+            menuBox.setAttribute('view', 'show');
+
+            setTimeout(() => {
+                menu.style.left = '0px';
+            }, 100);
+            
+        } else {
+            
+            setTimeout(() => {
+                menu.style.left = '300px';
+            }, 100);
+
+            setTimeout(() => {
+                menuBox.setAttribute('view', 'close');
+            }, 400);
+ 
+        }
     }
 }
 
